e02463e6a50d2ad91b6f83adc05b587362d911c1
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-07-18 13:07:39 +0300

    test branch

--- src/client/modules/home/index.js
+++ src/client/modules/home/index.js
@@ -16,22 +16,22 @@ function RouteConfig($routeProvider) {
 function MainCtrl($scope, $uibModal, $log, $location, $http, $window) {
 
     //<------------slider------------->
-    $scope.myInterval = 2000;
-    $scope.noWrapSlides = false;
-    $scope.active = 0;
-    $scope.noPause = false;
-
-    var slides = $scope.slides = [];
-    var currIndex = 0;
-
-    $scope.addSlide = function(imageNum) {
-        slides.push({
-            image: 'public/images/home_' + imageNum + '.png',
-            id: currIndex++
-        });
-    };
-
-    for (var i = 0; i < 3; i++) {
-        $scope.addSlide("0" + (i + 1));
-    }
+    // $scope.myInterval = 2000;
+    // $scope.noWrapSlides = false;
+    // $scope.active = 0;
+    // $scope.noPause = false;
+    //
+    // var slides = $scope.slides = [];
+    // var currIndex = 0;
+    //
+    // $scope.addSlide = function(imageNum) {
+    //     slides.push({
+    //         image: 'public/images/home_' + imageNum + '.png',
+    //         id: currIndex++
+    //     });
+    // };
+    //
+    // for (var i = 0; i < 3; i++) {
+    //     $scope.addSlide("0" + (i + 1));
+    // }
 }
