34c4a7c100e494af49cb038512c19e0c144493bc
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-07-25 19:46:15 +0300

    change

--- test/client/modules/dashboard/test.spec.js
+++ test/client/modules/dashboard/test.spec.js
@@ -1,29 +1,46 @@
-"use strict";
-describe("Testing dashboard", function () {
-    // var injector, $httpBackend, $controller;
-    //
-    // beforeEach(angular.mock.module("tanks.dashboard"));
-    //
-    // beforeEach(() => {
-    //     inject(($injector, _$controller_, _$httpBackend_) => {
-    //         injector = $injector;
-    //         // $httpBackend = $injector.get('$httpBackend');
-    //         $controller = _$controller_;
-    //         $httpBackend = _$httpBackend_;
-    //         $httpBackend.when('GET', 'api/users').respond({users: []});
-    //     });
-    // });
-    //
-    // afterEach(() => {
-    //     $httpBackend.verifyNoOutstandingExpectation();
-    //     $httpBackend.verifyNoOutstandingRequest();
-    // });
-    //
-    // it("calls api/places", inject(($http) => {
-    //     var $scope = {};
-    //     $httpBackend.expectGET('api/users');
-    //     var controller = $controller('DashboardCtrl', { $scope: $scope, $http: $http });
-    //     expect($scope.var1).toEqual(5);
-    //     $httpBackend.flush();
-    // }));
+// describe("Testing dashboard", function () {
+//     var injector, $httpBackend, $controller;
+
+//     beforeEach(angular.mock.module("tanks.dashboard"));
+
+//     beforeEach(() => {
+//         inject(($injector, _$controller_, _$httpBackend_) => {
+//             injector = $injector;
+//             // $httpBackend = $injector.get('$httpBackend');
+//             $controller = _$controller_;
+//             $httpBackend = _$httpBackend_;
+//             $httpBackend.when('GET', 'api/users').respond({users: []});
+//         });
+//     });
+
+//     afterEach(() => {
+//         $httpBackend.verifyNoOutstandingExpectation();
+//         $httpBackend.verifyNoOutstandingRequest();
+//     });
+
+/*
+<<<<<<< HEAD
+    it("calls api/places", inject(($http) => {
+        var $scope = {};
+        $httpBackend.expectGET('api/users');
+        var controller = $controller('DashboardCtrl', { $scope: $scope, $http: $http });
+        expect($scope.var1).toEqual(5);
+        $httpBackend.flush();
+    }));
 });
+=======
+>>>>>>> module_send_data
+//     fit("calls api/places", inject(($http) => {
+//         var $scope = {};
+//         $httpBackend.expectGET('api/users');
+//         var controller = $controller('DashboardCtrl', { $scope: $scope, $http: $http });
+//         expect($scope.var1).toEqual(5);
+//         $httpBackend.flush();
+//     }));
+// });
+<<<<<<< HEAD
+
+=======
+>>>>>>> second_tank
+*/
+
